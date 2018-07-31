24c92fd332f9da3cc46df9609df4fe03b2052e75
Alisson Gomes <alisson.php@gmail.com>
2018-07-30 23:07:51 -0300

    feat(routes): Add routes to news instances

--- start/routes.js
+++ start/routes.js
@@ -14,7 +14,18 @@ Route.group(
     Route.resource('users', 'UserController').apiOnly()
     Route.post('restaurants/tables', 'RestaurantController.addTable').as('restaurants.table.add')
     Route.resource('restaurants', 'RestaurantController').apiOnly()
+    Route.resource('categories', 'MenuCategoryController').apiOnly()
+    Route.resource('items', 'MenuItemController').apiOnly()
+    Route.resource('events', 'EventController').apiOnly()
+    Route.resource('points', 'UserPointController').apiOnly()
   })
   .prefix('api/v1')
   .namespace('Api/v1')
   .middleware(['auth', 'acl:admin'])
+
+Route.group(
+  () => {
+    Route.get('restaurants/tables/qrcode/:id', 'RestaurantController.qrcode').as('restaurants.table.qrcode')
+  })
+  .prefix('drive')
+  .namespace('Api/v1')
