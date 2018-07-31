b9f1c20be91487bc70bc4d15f1b21aeb8c5b4ef3
Alisson Gomes <alisson.php@gmail.com>
2018-07-30 23:05:34 -0300

    feat(seeds): Add new models

--- database/factory.js
+++ database/factory.js
@@ -39,3 +39,53 @@ Factory.blueprint('App/Models/RestaurantTables', async (faker) => {
     active: faker.bool()
   }
 })
+
+Factory.blueprint('App/Models/MenuCategory', async (faker) => {
+  return {
+    category: faker.word()
+  }
+})
+
+Factory.blueprint('App/Models/MenuItem', async (faker, i, data) => {
+  return {
+    restaurants_id: data.restaurants_id,
+    menu_categories_id: data.categories_id,
+    title: faker.sentence(),
+    description: faker.paragraph(),
+    photo: faker.avatar(),
+    price: faker.floating({ min: 0, max: 160 }),
+    serves: faker.natural({ min: 1, max: 5 }),
+    shrimp: faker.bool(),
+    chili: faker.bool(),
+    chili_level: faker.natural({ min: 1, max: 3 }),
+    active: faker.bool(),
+  }
+})
+
+Factory.blueprint('App/Models/Event', async (faker, i, data) => {
+  return {
+    restaurants_id: data.restaurants_id,
+    event: faker.sentence(),
+    description: faker.paragraph(),
+    cover: faker.avatar(),
+    datetime: faker.date(),
+    price: faker.floating({ min: 0, max: 80 }),
+    ticket: faker.bool(),
+    active: faker.bool()
+  }
+})
+
+Factory.blueprint('App/Models/UserPoint', async (faker, i, data) => {
+  return {
+    user_id: data.user_id,
+    operation: faker.pickone(
+      [
+        'input',
+        'output'
+      ]
+    ),
+    points: faker.floating({ min: -80, max: 80 }),
+    consumption: faker.floating({ min: 30, max: 380 }),
+    obs: faker.paragraph()
+  }
+})
